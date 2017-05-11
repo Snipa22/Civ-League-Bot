f10e249be1bf01c5c4267c7aaaba647fd875ac3c
© • Song Adieu <jeThomas.Two@Gmail.com>
2017-05-11 13:42:20 -0400

    Updated Node

--- CLS.js
+++ CLS.js
@@ -1,7 +1,9 @@
 "use strict";
 /*Author: © • Song Adieu | Created: March, 2017*/
-
+//const commando = require('discord.js-commando');
+//const path = require('path');
 const Discord = require("discord.js");
+const Config = require("./config.json");
 const Players = require("./players.json");
 
 const CivLeagueStats = new Discord.Client();
@@ -18,9 +20,22 @@ CivLeagueStats.on("message", message => {
     command = command.slice(Config.dot.length);
     let args = message.content.split(" ").splice(1);
 
-    //Song Adieu
-    if (command === "SongAdieu") {
+/*  --  Player List  --
+    if(command === 'SongAdieu') {
+        let player0 = new Discord.RichEmbed()
+            .setThumbnail(message.author.avatarURL)
 
-    }
+            .addField("Wins:", "262", true)
+            .addField("Losses:", "117", true)
+            .addField("Games Played:", "92", true)
+            .setFooter("•|• 𝔉𝔬𝔯𝔴𝔞𝔯𝔡 𝔖𝔢𝔱𝔱𝔩𝔢, 𝔶𝔬𝔲 𝔇𝔦𝔢 •|•")
+            .setColor(0xB16EFE)
+        message.channel.sendEmbed(player0);
+    };*/
+
+/*  --  Player List  --  */
+
+
 });
 
+CivLeagueStats.login(Config.tokens.StatsBot);
